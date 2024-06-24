273a3432b4a9eca05396c2e37ec231493e21c581
William Walsh <william.stephen.walsh@gmail.com>
2024-06-24 11:25:46 +1000

    Add priority annotation example.

--- src/test/java/ie/williamswalsh/PriorityTest.java
+++ src/test/java/ie/williamswalsh/PriorityTest.java
@@ -1,2 +1,26 @@
-package ie.williamswalsh;public class PriorityTest {
+package ie.williamswalsh;
+
+import org.testng.annotations.Test;
+
+public class PriorityTest {
+
+    @Test(priority = 1)
+    void testOne() {
+        System.out.println("testOne output - priority = 1");
+    }
+
+    @Test(priority = 2)
+    void testTwo() {
+        System.out.println("testTwo output - priority = 2");
+    }
+
+    @Test(priority = 3, enabled = false)
+    void testThree() {
+        System.out.println("testThree output - priority = 3");
+    }
+
+    @Test(priority = 4)
+    void testFour() {
+        System.out.println("testFour output - priority = 4");
+    }
 }
