1e80fd1eec2a388143aaab1c8214480c20e002fa
William Walsh <william.stephen.walsh@gmail.com>
2024-06-24 11:26:46 +1000

    Add grouping and dependent tests - files.

--- src/test/java/ie/williamswalsh/DependencyTest.java
+++ src/test/java/ie/williamswalsh/DependencyTest.java
@@ -1,2 +1,30 @@
-package ie.williamswalsh;public class DependencyTest {
+package ie.williamswalsh;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DependencyTest {
+
+    @Test
+    public void testStartCar() {
+        System.out.println("Start car");
+        Assert.fail("Demo failure to show ignoring of dependent test methods.");
+    }
+
+    @Test(dependsOnMethods = {"testStartCar"})
+    public void testDriveCar() {
+        System.out.println("Drive car");
+    }
+
+    @Test(dependsOnMethods = {"testDriveCar"})
+    public void testStopCar() {
+        System.out.println("Stop car");
+    }
+
+//    testStopCar - not the clearest example
+//    Just wants to demo the multiple dependent tests
+    @Test(dependsOnMethods = {"testStopCar","testStartCar"})
+    public void testParkCar() {
+        System.out.println("Park Car");
+    }
 }
--- src/test/java/ie/williamswalsh/GroupingExample.java
+++ src/test/java/ie/williamswalsh/GroupingExample.java
@@ -1,2 +1,31 @@
-package ie.williamswalsh;public class GroupingExample {
+package ie.williamswalsh;
+
+import org.testng.annotations.Test;
+
+public class GroupingExample {
+
+    @Test(groups = {"sanity"})
+    void test1() {
+        System.out.println("Test 1 output");
+    }
+
+    @Test(groups = {"sanity"})
+    void test3() {
+        System.out.println("Test 3 output");
+    }
+
+    @Test(groups = {"regression"})
+    void test2() {
+        System.out.println("Test 2 output");
+    }
+
+    @Test(groups = {"regression"})
+    void test4() {
+        System.out.println("Test 4 output");
+    }
+
+    @Test(groups = {"sanity", "regression"})
+    void test5() {
+        System.out.println("Test 5 output");
+    }
 }
